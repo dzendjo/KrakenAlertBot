c302719f758670226323d9f459f9c1afc3753de5
andrii.belon@gmail.com <andrii.belon@gmail.com>
2018-02-27 17:41:04 +0200

    *Add db procedures

--- KrakenAlertBot.py
+++ KrakenAlertBot.py
@@ -1,38 +1,79 @@
 import requests
 from sqlalchemy import create_engine
 from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    telegram_id = Column(String)
+
+    def __init__(self, telegram_id):
+        self.telegram_id = telegram_id
+
+    def __repr__(self):
+        return '{} - {}'.format(self.id, self.telegram_id)
+
+
+class Pair(Base):
+    __tablename__ = 'pairs'
+    id = Column(Integer, primary_key=True)
+    pair = Column(String)
+
+    def __init__(self, pair):
+        self.pair = pair
+
+    def __repr__(self):
+        return '{} - {}'.format(self.id, self.pair)
+
+
+class Order(Base):
+    __tablename__ = 'orders'
+    id = Column(Integer, primary_key=True)
+    pair_id = Column(Integer, ForeignKey("pairs.id"))
+    user_id = Column(Integer, ForeignKey("users.id"))
+    price = Column(Integer)
+
+    pair = relationship("Pair", foreign_keys=[pair_id])
+    user = relationship("User", foreign_keys=[user_id])
+
+    def __init__(self, pair, user, price):
+        self.pair = pair
+        self.user = user
+        self.price = price
+
+    def __repr__(self):
+        return '{}: {}, {}, {}'.format(self.id, self.pair.pair, self.user.telegram_id, self.price)
 
 
 class KrakenAlertBot:
     def __init__(self):
-        self.db_engine = create_engine('sqlite:///db.sqlite', echo=True)
-        metadata = MetaData()
-        users_table = Table('users', metadata,
-                            Column('id', Integer, primary_key=True),
-                            Column('telegram_id', String)
-                            )
+        db_engine = create_engine('sqlite:///db.sqlite', echo=True)
+        self.session = sessionmaker(bind=db_engine)()
 
-        pairs_table = Table('pairs', metadata,
-                            Column('id', Integer, primary_key=True),
-                            Column('pair', String)
-                            )
+    def put_to_db(self, entity):
+        self.session.add(entity)
+        self.session.commit()
 
-        orders_table = Table('orders', metadata,
-                            Column('id', Integer, primary_key=True),
-                            Column('pair_id', Integer, ForeignKey('pairs.id')),
-                            Column('user_id', Integer, ForeignKey('users.id')),
-                            Column('price', Integer)
-                             )
+    def get_user_from_db(self, telegram_id):
+        return self.session.query(User).filter_by(telegram_id=telegram_id).first()
 
-        metadata.create_all(self.db_engine)
+    def get_pair_from_db(self, pair):
+        return self.session.query(Pair).filter_by(pair=pair).first()
 
+    def get_orders_from_db(self, telegram_id):
+        user = self.get_user_from_db(telegram_id)
+        return self.session.query(Order).filter_by(user=user).all()
 
-
-    def put_to_storage(self):
-        pass
-
-    def del_from_storage(self):
-        pass
+    def del_from_db(self, entity):
+        class_name = type(entity).__name__
+        self.session.query(class_name).filter_by(id=entity.id).delete
+        self.session.commit()
 
     def get_current_rate(self, pair):
         request = requests.get('https://api.kraken.com/0/public/Ticker?pair={}'.format(pair))
--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import KrakenAlertBot as alertbot
+from KrakenAlertBot import User, Pair, Order
 
 
 a = alertbot.KrakenAlertBot()
-print(a.get_current_rate('USDTUSD'))
+user1 = User('1234590111')
+
+print(a.get_orders_from_db('12345901'))
+# a.put_to_storage(user1)
+# user1 = User('12345901')
+# pair1 = a.session.query(Pair).filter_by(id=1).first()
+# a.session.add(Order(pair1, user1, 1.001))
+# print('op')
+#
+# a.session.commit()
